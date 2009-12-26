76ae51f0ee7fec371f992d6b55f6df4441e9cf62
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-26 23:19:35 +0000

    More helpers

--- lib/nake/helpers.rb
+++ lib/nake/helpers.rb
@@ -1,21 +1,61 @@
 # encoding: utf-8
 
 require "fileutils"
+require "nake/colors"
 
 module Nake
   module TaskHelpers
     include FileUtils
-    alias_method :run, :`
+    def run(*parts)
+      puts parts.join(" ").cyan
+      system(*parts)
+    end
 
     # return true if process suceeded, false otherwise
     # sh "ls -a"
     # sh "ls", "-a"
     def sh(*parts)
       puts "#{"$".magenta} #{parts.join(" ").cyan}"
-      run "#{parts.shift} #{parts.join(" ")}"
-      $?.success?
+      system("sh", "-c", *parts)
+    end
+
+    # zsh "ls .*(.)"
+    def zsh(*parts)
+      puts "#{"%".magenta} #{parts.join(" ").cyan}"
+      system("zsh", "-c", *parts)
+    end
+  end
+
+  module PrintHelpers
+    def info(message)
+      puts "~ #{message}".cyan if Nake.verbose
+    end
+
+    def note(message)
+      puts "~ #{message}".green if Nake.verbose
+    end
+
+    def warn(message)
+      Kernel.warn("~ #{message.yellow}") if Nake.verbose
+    end
+
+    def error(message)
+      STDERR.puts "~ #{message}".red if Nake.verbose
+    end
+
+    def abort(message)
+      Kernel.abort "[#{"ERROR".red}] #{message}"
+    end
+
+    def print_exception_and_abort(exception)
+      abort exception.message
+    end
+
+    def print_exception_with_backtrace_and_abort(exception)
+      abort [exception.message, exception.backtrace.join("- ")].join("\n")
     end
   end
 end
 
 Object.send(:include, Nake::TaskHelpers)
+Object.send(:include, Nake::PrintHelpers)
