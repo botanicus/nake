341b8102d0413d28d6dd6e7007ccb55754c4df86
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-28 17:13:50 +0100

    Added code-cleaner as a dependency for contributors

--- tasks.rb
+++ tasks.rb
@@ -5,6 +5,12 @@ require "nake/tasks/gem"
 require "nake/tasks/spec"
 require "nake/tasks/release"
 
+begin
+  load "code-cleaner.nake"
+rescue LoadError
+  warn "If you want to contribute to nake, you have to install code-cleaner gem and then run ./tasks.rb hooks:whitespace:install to get Git pre-commit hook for removing trailing whitespace and improving code quality in general."
+end
+
 Task[:build].config[:gemspec] = "nake.gemspec"
 Task[:prerelease].config[:gemspec] = "nake.pre.gemspec"
 Task[:release].config[:name] = "nake"
