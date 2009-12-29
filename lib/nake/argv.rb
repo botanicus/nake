2ebf85fb311791da97512624403ebc8138db49cf
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-29 01:10:04 +0100

    Reworked ARGV parsing

--- lib/nake/argv.rb
+++ lib/nake/argv.rb
@@ -1,28 +1,35 @@
 # encoding: utf-8
 
 module Nake
-  module ArgvParsingMixin
-    def extract!
-      self.inject(Hash.new) do |options, argument|
-        case argument
-        when /^--no-([^=]+)$/ # --no-git-repository
-          options[$1.gsub("-", "_").to_sym] = false
-          self.delete(argument)
-        when /^--([^=]+)$/    # --git-repository
-          options[$1.gsub("-", "_").to_sym] = true
-          self.delete(argument)
-        when /^--([^=]+)=([^,]+)$/ # --controller=post
-          key, value = $1, $2
-          options[key.gsub("-", "_").to_sym] = value.dup
-          self.delete(argument)
-        when /^--([^=]+)=(.+)$/    # --controllers=posts,comments
-          key, value = $1, $2
-          options[key.gsub("-", "_").to_sym] = value.split(",")
-          self.delete(argument)
-        else
-          # just extract options and ignore others
+  module ArgvParser
+    def self.extract(args)
+      args.inject(Hash.new) do |options, argument|
+        key, value = self.parse(argument)
+        if key
+          options[key] = value
         end
         options
+      end
+    end
+
+    def self.parse!(argument)
+      self.parse(argument) || raise("Argument #{argument} can't be parsed!")
+    end
+
+    def self.parse(argument)
+      case argument
+      when /^--no-([^=]+)$/ # --no-git-repository
+        return [$1.gsub("-", "_").to_sym, false]
+      when /^--([^=]+)$/    # --git-repository
+        return [$1.gsub("-", "_").to_sym, true]
+      when /^--([^=]+)=([^,]+)$/ # --controller=post
+        key, value = $1, $2
+        return [key.gsub("-", "_").to_sym, value.dup]
+      when /^--([^=]+)=(.+)$/    # --controllers=posts,comments
+        key, value = $1, $2
+        return [key.gsub("-", "_").to_sym, value.split(",")]
+      else
+        return false
       end
     end
   end
