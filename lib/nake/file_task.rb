e913d3c34a5da2891eaad134e3ea307a75436a52
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-29 03:17:30 +0100

    Improved file task

--- lib/nake/file_task.rb
+++ lib/nake/file_task.rb
@@ -4,31 +4,48 @@ require "nake/task"
 
 module Nake
   class FileTask < Task
+    alias_method :path, :name
+
     def call(args = Array.new, options = Hash.new)
+      will_run = true
       unless self.dependencies.empty?
-        puts "~ Invoking file task #{name}".cyan
+        info "Invoking file task #{name}"
         will_run = self.invoke_dependencies(*args, options)
       end
       if will_run && ! self.blocks.empty?
-        puts "~ Executing file task #{name} with arguments #{args.inspect} and options #{options.inspect}".green
+        note "Executing file task #{name} with arguments #{args.inspect} and options #{options.inspect}"
+        debug "Config #{self.config.inspect}"
         self.blocks.each do |block|
-          if block.arity.eql?(0)
-            block.call
-          else
-            block.call(*args, options)
-          end
+          block.call(self.path, *args, options)
         end
+      else
+        warn "File task #{name} won't be executed"
       end
     end
 
     protected
+    # task will be executed just if
+    def will_run?
+      file_tasks = self.dependencies.select { |name| self.class[name] }
+      file_tasks.any? do |path|
+
+      end
+    end
+
+    def task_will_run?(path)
+      File.exist?(path) || File.mtime(path) < File.mtime(self.name)
+    end
+
     # return true if the task need to be executed, false otherwise
     def invoke_dependencies(*args, options)
-      self.dependencies.all? do |name|
-        task = self.class[name] # first try if there is a task with given name
-        task.call(*args, options) # TODO: what about arguments?
-        File.exist?(name) # if not, then fallback to file
-        File.mtime(name) < File.mtime(self.name)
+      self.dependencies.each do |name|
+        if task = self.class[name] # first try if there is a task with given name
+          task.call(*args, options) # TODO: what about arguments?
+        elsif File.exist?(name) # if not, then fallback to file
+          task_will_run?(name)
+        else
+          raise TaskNotFound, "Task with name #{name} doesn't exist"
+        end
       end
     end
   end
