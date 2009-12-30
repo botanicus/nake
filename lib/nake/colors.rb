7f524dd874ac271a42a5112b9c878b7686d914a8
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-31 00:03:36 +0100

    Use colors just for an interactive sessions

--- lib/nake/colors.rb
+++ lib/nake/colors.rb
@@ -7,3 +7,7 @@ rescue LoadError
 end
 
 String.send(:include, Term::ANSIColor) # just for now, I know it's a mess
+
+# show colors just if you run nake directly from command line,
+# but not if are using nake in a script or so
+Term::ANSIColor.coloring = STDIN.tty?
