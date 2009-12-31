dc285c56da94ab9748c066ca77218ff4cc2bc77d
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2009-12-31 20:33:53 +0100

    DSL helpers namespaced into Nake::DSL

--- lib/nake/dsl.rb
+++ lib/nake/dsl.rb
@@ -4,60 +4,67 @@ require "nake"
 require "nake/task"
 require "nake/file_task"
 
-class Object
-  include Nake
-  def argument(*names, &block)
-    Nake.args[*names] = block
-  end
+module Nake
+  module DSL
+    include Nake
+    def argument(*names, &block)
+      Nake.args[*names] = block
+    end
 
-  # desc :release, "Create a new release"
-  # task(:release) do
-  #   # ...
-  # end
-  def desc(task, description)
-    Task.new(task).description = description
-  end
+    # desc :release, "Create a new release"
+    # task(:release) do
+    #   # ...
+    # end
+    def desc(task, description)
+      Task.new(task).description = description
+    end
 
-  # Rake-style task definition
-  # task(:release, :build) do |task|
-  #   # task definition
-  # end
-  def task(name, *dependencies, &block)
-    if block.nil?
-      Task.new(name, *dependencies)
-    else
-      Task.new(name, *dependencies) do |task|
-        task.define(&block)
+    # Rake-style task definition
+    # task(:release, :build) do |task|
+    #   # task definition
+    # end
+    def task(name, *dependencies, &block)
+      if block.nil?
+        Task.new(name, *dependencies)
+      else
+        Task.new(name, *dependencies) do |task|
+          task.define(&block)
+        end
       end
     end
-  end
 
-  def file(path, *dependencies, &block)
-    if block.nil?
-      FileTask.new(path, *dependencies)
-    else
-      FileTask.new(path, *dependencies) do |task|
-        task.define(&block)
+    def file(path, *dependencies, &block)
+      if block.nil?
+        FileTask.new(path, *dependencies)
+      else
+        FileTask.new(path, *dependencies) do |task|
+          task.define(&block)
+        end
       end
     end
-  end
 
-  # rule "*.o", "*.c"
-  # rule "**/*.o", "**/.c"
-  def rule(glob, dependency, &block)
-    Dir.glob(glob).each do |path|
+    # rule "*.o", "*.c"
+    # rule "**/*.o", "**/.c"
+    def rule(pattern, *dependencies, &block)
+      if block.nil?
+        Rule.new(pattern, *dependencies)
+      else
+        Rule.new(pattern, *dependencies) do |rule|
+          rule.define(&block)
+        end
+      end
+    end
+
+    def directory(path)
       FileTask.new(path) do |task|
-        task.description = "Generate #{path}"
-        task.define(&block)
+        task.hidden = true # do not show in list
+        task.description = "Create directory #{path}"
+        task.define { mkdir_p path }
       end
     end
   end
+end
 
-  def directory(path)
-    FileTask.new(path) do |task|
-      task.hidden = true # do not show in list
-      task.description = "Create directory #{path}"
-      task.define { mkdir_p path }
-    end
-  end
+class Object
+  include Nake::DSL
 end
