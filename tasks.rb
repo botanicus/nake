71d96b7b062657dbcbb46583f43e95d3cd104bf6
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-02 03:25:56 +0100

    Fixed NoMethodError if code-cleaner isn't installed

--- tasks.rb
+++ tasks.rb
@@ -7,6 +7,10 @@ require "nake/tasks/release"
 
 begin
   load "code-cleaner.nake"
+  Nake::Task["hooks:whitespace:install"].tap do |task|
+    task.config[:encoding] = "utf-8"
+    task.config[:whitelist] = '(bin/[^/]+|.+\.(rb|rake|nake|thor|task))$'
+  end
 rescue LoadError
   warn "If you want to contribute to nake, you have to install code-cleaner gem and then run ./tasks.rb hooks:whitespace:install to get Git pre-commit hook for removing trailing whitespace and improving code quality in general."
 end
@@ -15,11 +19,6 @@ Task[:build].config[:gemspec] = "nake.gemspec"
 Task[:prerelease].config[:gemspec] = "nake.pre.gemspec"
 Task[:release].config[:name] = "nake"
 Task[:release].config[:version] = Nake::VERSION
-
-Nake::Task["hooks:whitespace:install"].tap do |task|
-  task.config[:encoding] = "utf-8"
-  task.config[:whitelist] = '(bin/[^/]+|.+\.(rb|rake|nake|thor|task))$'
-end
 
 Task.new(:features) do |task|
   task.description = "Run cucumber scenarios"
