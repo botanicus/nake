67d82b0fd91bfb00006959470a76a8ba798f90e7
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-22 00:53:47 +0000

    Fixed spec:stubs

--- lib/nake/tasks/spec.rb
+++ lib/nake/tasks/spec.rb
@@ -3,7 +3,7 @@
 # ./tasks.rb spec/nake/argv_spec.rb spec/nake/task_spec.rb
 task(:spec) do |*paths, options|
   paths.push("spec") if paths.empty?
-  sh "spec #{paths.join(" ")}"
+  sh "spec", *paths
 end
 
 Task.new("spec:stubs") do |task|
@@ -12,18 +12,8 @@ Task.new("spec:stubs") do |task|
     Dir.glob("lib/**/*.rb").each do |file|
       specfile = file.sub(/^lib/, "spec").sub(/\.rb$/, '_spec.rb')
       unless File.exist?(specfile)
-        %x[mkdir -p #{File.dirname(specfile)}]
-        %x[touch #{specfile}]
-        puts "Created #{specfile}"
-      end
-    end
-    (Dir.glob("spec/rango/**/*.rb") + ["spec/rango_spec.rb"]).each do |file|
-      libfile = file.sub(/spec/, "lib").sub(/_spec\.rb$/, '.rb')
-      if !File.exist?(libfile) && File.zero?(file)
-        %x[rm #{file}]
-        puts "Removed empty file #{file}"
-      elsif !File.exist?(libfile)
-        puts "File exists just in spec, not in lib: #{file}"
+        sh "mkdir -p #{File.dirname(specfile)}"
+        sh "touch #{specfile}"
       end
     end
   end
